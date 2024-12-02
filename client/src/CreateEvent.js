747cde034f55ad1d026cba889db66e5eca6a2218
nmathern <nam325@lehigh.edu>
2024-12-02 17:28:20 -0500

    placeholder button to create event

--- client/src/CreateEvent.js
+++ client/src/CreateEvent.js
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react';
 
 const CreateEvent = () => {
-
+  const handleCreateEvent = () => {
+    //create event functionality
+  }
   return (
     <div>
       <h2>Create events here</h2>
+      <button onClick={handleCreateEvent} className="create-event-button">
+        Create New Event
+      </button>
     </div>
   );
 };
